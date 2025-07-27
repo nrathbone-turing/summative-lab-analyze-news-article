2c86fb5eb8a171f2735ebba798b3613e900a6918
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-26 20:52:43 -0600

    Initial commit with skeleton structure for functions

---
+++ pythonAssessment.py
@@ -0,0 +1,32 @@
+import re
+from collections import Counter
+from pathlib import Path
+
+# Function to load in and read the data from an input
+def read_text_file(file_path):
+    None
+
+# Function to count the number of times a specific word is used in the text.
+def count_specific_word(text, word):
+    None
+
+# Function to identify the most common word in the text.
+def identify_most_common_word(text):
+        None
+
+# Function to calculate the average length of words in the text.
+# exclude punctuation and special characters
+def calculate_average_word_length(text):
+     None
+
+# Function to count the number of paragraphs in the text.
+# define paragraphs based on empty line breaks
+def count_paragraphs(text):
+     None
+
+# Function to count the number of sentences in the text.
+# define sentences based on punctuation marks
+def count_sentences(text):
+     None
+
+# Main function to display and print results
